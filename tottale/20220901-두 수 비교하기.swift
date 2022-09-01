4f343d7f5a807f0d29539051777324efa3279b94
tottal <66786418+tottalE@users.noreply.github.com>
2022-09-01 15:04:45 +0900

    Create 20220901-두 수 비교하기.swift

---
+++ tottale/20220901-두 수 비교하기.swift
@@ -0,0 +1,9 @@
+let input = readLine()!.split(separator: " ").map { Int($0)! }
+
+if input[0] > input[1] {
+    print(">")
+} else if input[0] > input[1] {
+    print("<")
+} else {
+    print("==")
+}
