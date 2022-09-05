f5d637c55ca47a7b2e819fb573ce56f1a69ad952
GUNDY <106914201+Gundy93@users.noreply.github.com>
2022-09-05 09:28:56 +0900

    Create 220905_사칙연산.swift

---
+++ gundy/220905_사칙연산.swift
@@ -0,0 +1,7 @@
+if let numbers = readLine()?.split(separator: " "), let A = Int(numbers[0]), let B = Int(numbers[1]) {
+    print(A+B)
+    print(A-B)
+    print(A*B)
+    print(A/B)
+    print(A%B)
+}
