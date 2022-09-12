a561e40bdddb8ff9f5b5936a1c146b1287c5be6a
GUNDY <106914201+Gundy93@users.noreply.github.com>
2022-09-13 08:03:57 +0900

    Update 220907_3Sum.swift

--- gundy/220907_3Sum.swift
+++ gundy/220907_3Sum.swift
@@ -1,54 +1,63 @@
 class Solution {
     func threeSum(_ nums: [Int]) -> [[Int]] {
+        var numbers: [Int] = nums
+        let firstSet: Set<Int> = Set(numbers)
+        firstSet.forEach {
+            if let lastIndex = numbers.lastIndex(of: $0) {
+                numbers.remove(at: lastIndex)
+            }
+        }
+        let secondSet: Set<Int> = Set(numbers)
+        secondSet.forEach {
+            if let lastIndex = numbers.lastIndex(of: $0) {
+                numbers.remove(at: lastIndex)
+            }
+        }
+        let thirdSet: Set<Int> = Set(numbers)
+        numbers = (Array(firstSet) + Array(secondSet) + Array(thirdSet)).sorted()
         var resultSet: Set<Set<Int>> = []
         var result: [[Int]] = []
-        let firstSet: Set<Int> = Set(nums)
-        var firstArray: [Int] = nums
-        for number in firstSet {
-            if let firstIndex = firstArray.firstIndex(of: number) {
-                firstArray.remove(at: firstIndex)
+        for first in 0...numbers.count - 3 {
+            if numbers[first] > 0 {
+                break
             }
-        }
-        let secondSet: Set<Int> = Set(firstArray)
-        for number in secondSet {
-            if let firstIndex = firstArray.firstIndex(of: number) {
-                firstArray.remove(at: firstIndex)
-            }
-        }
-        let thirdSet: Set<Int> = Set(firstArray)
-        let lastArray: [Int] = Array(firstSet) + Array(secondSet) + Array(thirdSet)
-        if lastArray.count > 2 {
-            for first in 0...lastArray.count-3 {
-                var second: Int = first + 1
-                while second < lastArray.count-1 {
-                    if lastArray[first] + lastArray[second] > Int.max ||
-                        lastArray[first] + lastArray[second] < Int.min {
-                        continue
-                    }
-                    var third = second + 1
-                    while third < lastArray.count {
-                        if lastArray[first] + lastArray[second] + lastArray[third] == 0 {
-                            resultSet.insert([lastArray[first], lastArray[second], lastArray[third]])
-                        }
-                        third += 1
-                    }
+            var second: Int = first + 1
+            while second < numbers.count - 1 {
+                if numbers[first] + numbers[second] < -100000 {
                     second += 1
+                    continue
                 }
+                if numbers[first] + numbers[second] > 0 {
+                    break
+                }
+                var third: Int = second + 1
+                while third < numbers.count {
+                    if numbers[first] + numbers[second] + numbers[third] == 0 {
+                        resultSet.insert([numbers[first], numbers[second], numbers[third]])
+                        if numbers[second] == numbers[third] {
+                            break
+                        }
+                    } else if numbers[first] + numbers[second] + numbers[third] > 0 {
+                        break
+                    }
+                    third += 1
+                }
+                second += 1
             }
         }
-        if resultSet.count > 0 {
-            resultSet.forEach{
-                if $0.count == 2 {
-                    if Array($0)[0] + Array($0)[1] * 2 == 0 {
-                        result.append([Array($0)[0], Array($0)[1], Array($0)[1]])
-                    } else {
-                        result.append([Array($0)[0], Array($0)[0], Array($0)[1]])
-                    }
-                } else if $0.count == 1 {
-                    result.append([0, 0, 0])
+        resultSet.forEach {
+            switch $0.count {
+            case 1:
+                result.append([0, 0, 0])
+            case 2:
+                if Array($0)[0] + (2 * Array($0)[1]) == 0 {
+                    result.append([Array($0)[0], Array($0)[1], Array($0)[1]])
                 } else {
-                result.append(Array($0))}
+                    result.append([Array($0)[0], Array($0)[0], Array($0)[1]])
                 }
+            default:
+                result.append(Array($0))
+            }
         }
         return result
     }
