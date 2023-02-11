39db9c51f10f977fd24cf78a2776dc93a708f662
김세영 <48676248+KimBaek-Seyeong@users.noreply.github.com>
2023-02-12 03:31:11 +0900

    Update 20230212_과일_장수.java

--- SY/20230212_과일_장수.java
+++ SY/20230212_과일_장수.java
@@ -1,1 +1,16 @@
+import java.util.*;
 
+class Solution {
+    public int solution(int k, int m, int[] score) {
+        int answer = 0;
+                
+        Arrays.sort(score);
+        for(int i=score.length-1; i>=0; i--) {
+            if((score.length - i) % m == 0) {
+                answer += score[i] * m;
+            }
+        }
+        
+        return answer;
+    }
+}
