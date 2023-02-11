879ea63628af0aad1a78e3f815b4011b9ac3a56b
김세영 <48676248+KimBaek-Seyeong@users.noreply.github.com>
2023-02-12 03:34:47 +0900

    Update 20230212_푸드 _파이트_대회.java

--- SY/20230212_푸드 _파이트_대회.java
+++ SY/20230212_푸드 _파이트_대회.java
@@ -1,1 +1,18 @@
 
+class Solution {
+    public String solution(int[] food) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=1; i<food.length; i++) {
+            int share = food[i] / 2;
+
+            for(int j=0; j<share; j++) {
+                sb.append(i);
+            }
+        }
+        
+        answer = sb.toString() + "0" + sb.reverse().toString();
+        return answer;
+    }
+}
