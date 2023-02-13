cd4bec7e25f90d205de247a9df33070d03bf010e
김세은 <107592967+seeunla@users.noreply.github.com>
2023-02-13 17:59:15 +0900

    Create 20230213_과일장수.java

---
+++ SE/20230213_과일장수.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int k, int m, int[] score) {
+        Arrays.sort(score);
+        int answer = 0;
+        int boxcount = score.length / m;
+        int index = score.length -1;
+        while (index >= 0) {
+            //System.out.println(index);
+            if (index -m +1 >= 0) {
+                answer += score[index -m + 1] * m;    
+            }
+            index = index - m;
+        }      
+        return answer;
+    }
+}
